acb1b58c5e7f95a0f976a0c8818cc65c4c120b44
Lidet s <luk4ls@yahoo.com>
2019-03-20 13:20:14 -0700

    models and routes

---
+++ backend/models/models.js
@@ -0,0 +1,73 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: {
+      type: String
+  },
+  password: {
+        type: String,
+  },
+  email: {
+       type: String,
+  },
+})
+
+
+var documentSchema = new Schema({
+    title: {
+        type: String,
+        default: 'Untitled Document'
+    },
+  author: {
+     type: mongoose.Schema.Types.ObjectId,
+     ref: 'User'
+  },
+  collaborators: [{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+  }],
+  content: String,
+  shareLink: String,
+  password: {
+         type: String,
+  },
+  dateCreated: String,
+  contentHistory: []
+})
+
+var Document = mongoose.model('Document', documentSchema);
+
+userSchema.methods.getOwnedDocuments = function (callback){
+  var userid = this._id;
+  Document.find({author: userid}).populate('author').populate('collaborators')
+  .exec(function(err,documents){
+      console.log('documents owned by user are are ', documents);
+    callback(err,documents);
+  })
+}
+userSchema.methods.getCollaboratedDocuments = function (callback){
+    var userid = this._id;
+    
+    Document.find({author: {$nin: [userid]}, collaborators: {$all: [userid]}}).populate('author').populate('collaborators')
+    .exec(function(err,documents){
+        console.log('documents only collaborate are ', documents);
+      callback(err,documents);
+    })
+  }
+
+userSchema.methods.getAllDocuments = function (callback){
+  var userid = this._id;
+  Document.find({collaborators: {$all: [userid]}}).populate('collaborators').populate('author')
+  .exec(function(err,documents){
+      console.log('documents are ', documents);
+    callback(err,documents);
+  })
+}
+
+
+var User = mongoose.model('User', userSchema);
+module.exports = {
+  User: User,
+  Document: Document
+ };
