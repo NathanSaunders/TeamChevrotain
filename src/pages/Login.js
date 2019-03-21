1c09bae93197e13cffb9d567182fb0476a89eb69
jadamsin3d <jadamsin3d@gmail.com>
2019-03-20 19:31:31 -0700

    changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LoginForm from '../components/LoginForm/LoginForm';
 
 class Login extends Component {
 	state = {
@@ -7,58 +8,30 @@ class Login extends Component {
 	};
 
 	onSubmit = (e) => {
+		e.preventDefault();
 		console.log('this was submitted');
-    };
-    
-    onChangeEmail = (e) => {
-        const {name, value} = e.target;
-        this.setState({
-            [name]: value
-        })
-    }
+	};
 
-    onChangePassword = (e) => {
-        const {name, value} = e.target;
-        this.setState({
-            [name]: value
-        })
-    }
+	onChangeEmail = (e) => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	};
+
+	onChangePassword = (e) => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	};
 
 	render() {
 		return (
 			<div>
-				<div>
-					<h1>Log-in to Chevrotain</h1>
-				</div>
-				<div className="container">
-					<div className="row">
-						<div className="col-md-12">
-							<form onSubmit={this.onSubmit}>
-								<div className="form-group">
-									<input
-										type="text"
-										name="email"
-										value={this.state.value}
-										onChange={this.onChangeEmail}
-										placeholder="Email"
-									/>
-								</div>
-								<div className="form-group">
-									<input
-										type="text"
-										name="password"
-										value={this.state.value}
-										onChange={this.onChangePassword}
-										placeholder="Password"
-									/>
-								</div>
-								<div className="form-group">
-									<input type="submit" value="Log-In" className="btn btn-primary" />
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
+                <LoginForm 
+                    {...this.state} 
+                />
 			</div>
 		);
 	}
