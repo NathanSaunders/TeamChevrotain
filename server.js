7a137fa973c59bffbec6851aefa7969e468f0883
Spencer Jack <spencer.jack@alaskaair.com>
2019-03-22 20:26:14 -0700

    updates from Spence

--- server.js
+++ server.js
@@ -6,17 +6,22 @@ const mongoose = require('mongoose');
 const passRoutes = express.Router();
 const PORT = 8080;
 
+// Requiring the `Document` model for accessing the `documents` collection
+var Document = require("./backend/models/models.js");
+
 let PWSchema = require('./backend/models/models');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/User', {useNewUrlParser: true});
+
+mongoose.connect('mongodb://localhost/snapDocsdb', {useNewUrlParser: true});
 const connection = mongoose.connection;
 
 connection.once('open', function() {
     console.log("Connected to Mongo Database");
 })
+
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
